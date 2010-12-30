6590d485e39531589bbabe668697635cb8d2cd00
Magnus Andersson <magnus@magnusart.com>
2010-12-30 19:21:03 +0100

    Added BroadcastReciever for use with BankDroid.
    Change-Id: Ib51d019353c41fcc62248f73e40fad0998705ea7
    Signed-off-by: Magnus Andersson <magnus@magnusart.com>

---
+++ src/com/magnusart/transtatistics/reciever/BankDroidTransactionReciever.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2010 Magnus Andersson 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.magnusart.transtatistics.reciever;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.magnusart.transtatistics.TranStatistics;
+
+/**
+ * This class recieves changes to transactions and makes sure they are stored
+ * for later processing.
+ * 
+ * @author Magnus Andersson
+ * @since 30 dec 2010
+ */
+public class BankDroidTransactionReciever extends BroadcastReceiver {
+
+	private static final String UPDATE_TRANSACTIONS = "com.magnusart.transtatistics.action.UPDATE_TRANSACTIONS";
+
+	/** {@inheritDoc} */
+	@Override
+	public void onReceive(final Context context, final Intent intent) {
+		Log.d(TranStatistics.TAG, "Recieved intent");
+
+		if (UPDATE_TRANSACTIONS.equals(intent.getAction())) {
+			Log.d(TranStatistics.TAG, "Begin retrieving updated transactions");
+			final String bankName = intent.getExtras().getString("bankName");
+			final String accountName = intent.getExtras().getString(
+					"accountName");
+			final String transactions[] = intent.getExtras().getStringArray(
+					"UpdatedTransactions");
+
+			for (final String t : transactions) {
+				try {
+					final JSONObject jObj = (JSONObject) new JSONTokener(t)
+							.nextValue();
+
+					Log.d(TranStatistics.TAG, jObj.toString(4));
+
+				} catch (final JSONException e) {
+					throw new RuntimeException(e);
+				}
+			}
+
+		}
+
+	}
+
+}
