c0a70c2e6f7e6a9666c486d341274c240b0ce1dc
Magnus Andersson <magnus@magnusart.com>
2010-12-30 19:27:21 +0100

    Added license and TAG.
    Change-Id: I60f00c80ed2f2ea2a5577d3bdca1db81a9c2b75c
    Signed-off-by: Magnus Andersson <magnus@magnusart.com>

--- src/com/magnusart/transtatistics/TranStatistics.java
+++ src/com/magnusart/transtatistics/TranStatistics.java
@@ -1,13 +1,36 @@
+/**
+ * Copyright 2010 Magnus Andersson 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.magnusart.transtatistics;
 
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * 
+ * @author Magnus Andersson
+ * @since 30 dec 2010
+ */
 public class TranStatistics extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	public static final String TAG = "TranStatistics";
+
+	/** {@inheritDoc} */
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
